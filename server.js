3d666261efe2e64cddc21c9dbc9ddaba729102cc
IDM.air <innanadm8@gmail.com>
2019-05-14 19:25:25 -0400

    adding more ports

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var http = require('http');
 
+var PORT = 5000;
 function handleRequest(request,response) {
 
-
+RTCIceTransportStateChangedEvent.end('yeaaaaah buddy! we made a server!')
 
 
 }
 
 var server = http.createServer(handleRequest); 
+
+server.listen(PORT, function () {
+    console.log('server has started on port' + PORT)
+   
+});
---
+++ server2.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var PORT = 7000;
+var PORT = 7500;
+
+function handleRequest(request, response) {
+
+    RTCIceTransportStateChangedEvent.end('yeaaaaah buddy! we made a server!')
+    
+}
